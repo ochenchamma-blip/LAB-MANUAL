25fea9b084d0bf6d1390f1127f5257fc3f6bd1c4
ochenchamma-blip <ochenchamma@gmail.com>
2026-07-25 09:55:30 +0530

    Add thread synchronization for ordered output
    Implemented a Foo class to ensure three threads execute in a fixed sequence, printing 'first', 'second', and 'third' in order.

---
+++ 4.Ensure three threads excute in a fixed sequence so thair output is always ordered (leedcode 1114-print in order).java
@@ -0,0 +1,48 @@
+Program:
+//1114-print in order
+class Foo {
+    
+    private volatile boolean onePrinted;
+    private volatile boolean twoPrinted;
+
+    public Foo() {
+        onePrinted = false;
+        twoPrinted = false;        
+    }
+
+    public synchronized void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        onePrinted = true;
+        notifyAll();
+    }
+
+    public synchronized void second(Runnable printSecond) throws InterruptedException {
+        while(!onePrinted) {
+            wait();
+        }
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        twoPrinted = true;
+        notifyAll();
+    }
+
+    public synchronized void third(Runnable printThird) throws InterruptedException {
+        while(!twoPrinted) {
+            wait();
+        }
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
+
+	
+
+
+
+
+
+
+
+ 
